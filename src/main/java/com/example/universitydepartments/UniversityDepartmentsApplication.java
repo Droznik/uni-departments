83bd5651fcf18fc63126a188b3710d7e8e419ca3
Dmytry Droznyk <dmytro.droznyk@itomy.ch>
2022-03-22 13:12:13 +0200

    initial

---
+++ src/main/java/com/example/universitydepartments/UniversityDepartmentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.universitydepartments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityDepartmentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityDepartmentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/universitydepartments/UniversityDepartmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.universitydepartments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityDepartmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
